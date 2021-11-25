e478f6707703fcb436a68c0ff8b998c02343ff3e
Monsingozzy <mohansinghyoyo88@gmail.com>
2021-11-26 04:02:30 +0530

    vote init

--- vote.js
+++ vote.js
@@ -1,51 +1,51 @@
-var j=0;
-var b=0;
-var c=0;
+var j = 0;
+var b = 0;
+var c = 0;
 
-function bjpfun(){
-  b=b+1
-  document.getElementById("rbjp").innerHTML=b;
-  
+function bjpfun() {
+    b = b + 1
+    document.getElementById("rbjp").innerHTML = b;
+
 }
 
-function cngfun(){
-    c=c+1;
-    document.getElementById("rcng").innerHTML=c;
+function cngfun() {
+    c = c + 1;
+    document.getElementById("rcng").innerHTML = c;
 }
 
-function jdsfun(){
-    j=j+1;
-    document.getElementById("rjds").innerHTML=j;
+function jdsfun() {
+    j = j + 1;
+    document.getElementById("rjds").innerHTML = j;
 }
 
-function resultt(){
-    var title="BJP jai modi"
-    if(b>j && b>c){
-        document.getElementById("winner").innerHTML=title;
+function resultt() {
+    var title = "BJP jai modi"
+    if (b > j && b > c) {
+        document.getElementById("winner").innerHTML = title;
     }
-    else{
-        if(c>b&&c>j){
-            var title="CNG jai pappu"
-            document.getElementById("winner").innerHTML=title;
+    else {
+        if (c > b && c > j) {
+            var title = "CNG jai pappu"
+            document.getElementById("winner").innerHTML = title;
 
         }
-        else{
-            if(j>b&& j>c){
-            var title2="JDS jai kswamy layout"
-            document.getElementById("winner").innerHTML=title2;
+        else {
+            if (j > b && j > c) {
+                var title2 = "JDS jai kswamy layout"
+                document.getElementById("winner").innerHTML = title2;
             }
-            else{
-                var title4="election was tie lets super over"
-                document.getElementById("winner").innerHTML=title4;
+            else {
+                var title4 = "election was tie lets super over hahaha ☺☺☺☻"
+                document.getElementById("winner").innerHTML = title4;
             }
 
         }
     }
- 
-   
 
 
-    
+
+
+
 }
 
 
