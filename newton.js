4466a09a04df6b18413a3ad069ab5a1099d037d2
Monsingozzy <mohansinghyoyo88@gmail.com>
2021-11-26 04:04:05 +0530

    inint

--- newton.js
+++ newton.js
@@ -1,10 +1,23 @@
 // var col=document.getElementById("btn")
+var clr;
 function make_dark(){
+   
     var col=document.querySelector("body");
     col.style.backgroundColor="black";
     col.style.color="white";
-    // clearInterval(clr,200);
+
 
   
 }
-// var clr=setTimeout(make_dark,200);
+// clr=setTimeout(make_dark,8000);
+// make_dark()
+
+// document.getElementById('School').addEventListener('click',function(){
+
+//     alert("clicked");
+
+
+// });
+document.getElementById("input").value;
+
+input.value=f2;
