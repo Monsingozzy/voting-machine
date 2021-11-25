12340b39664e0d80a863fbb36df5d0c1ebf03b03
Monsingozzy <mohansinghyoyo88@gmail.com>
2021-11-25 16:00:13 +0530

    init

---
+++ newton.js
@@ -0,0 +1,10 @@
+// var col=document.getElementById("btn")
+function make_dark(){
+    var col=document.querySelector("body");
+    col.style.backgroundColor="black";
+    col.style.color="white";
+    // clearInterval(clr,200);
+
+  
+}
+// var clr=setTimeout(make_dark,200);
